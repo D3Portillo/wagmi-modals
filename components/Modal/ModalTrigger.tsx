3bd768b2d4fa579d3e150f2fb0e385f8443dd7d2
D3Portillo <d3portillo@gmail.com>
2023-05-24 09:38:25 +0200

    enh(ModalTrigger.tsx): expect render prop for children

--- components/Modal/ModalTrigger.tsx
+++ components/Modal/ModalTrigger.tsx
@@ -1,22 +1,33 @@
-import type { PropsWithChildrenCx } from "@/types/shared"
 import type Modal from "./Modal"
-import React, { Fragment } from "react"
-import Button from "@/components/Button"
+import React, { Fragment, type ReactNode } from "react"
 import useOnOffMachine from "@/lib/useOnOffMachine"
+import Button from "@/components/Button"
+
+type RenderProp = (props: { open: () => void }) => JSX.Element
 
 function ModalTrigger({
   opens: ModalContent,
   children,
   className,
-}: PropsWithChildrenCx<{ opens: typeof Modal }>) {
+}: {
+  opens: typeof Modal
+  className?: string
+  children: ReactNode | RenderProp
+}) {
   const modalMachine = useOnOffMachine()
 
   return (
     <Fragment>
       <ModalContent onClose={modalMachine.turnOff} isOpen={modalMachine.isOn} />
-      <Button onClick={modalMachine.turnOn} className={className}>
-        {children}
-      </Button>
+      {typeof children === "function" ? (
+        children({
+          open: modalMachine.turnOn,
+        })
+      ) : (
+        <Button onClick={modalMachine.turnOn} className={className}>
+          {children}
+        </Button>
+      )}
     </Fragment>
   )
 }
